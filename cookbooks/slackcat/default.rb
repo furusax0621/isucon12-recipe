32d0c6570dc79f96e072121a08e7eac99a4dbd21
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2022-07-08 16:48:42 +0900

    とりあえず脳死で入れとけーというやつら

---
+++ cookbooks/slackcat/default.rb
@@ -0,0 +1,6 @@
+github_binary 'slackcat' do
+  repository 'bcicen/slackcat'
+  version '1.7.3'
+  archive "slackcat-1.7.3-linux-amd64"
+  binary_path 'slackcat-1.7.3-linux-amd64'
+end
---
+++ roles/ubuntu/default.rb
@@ -0,0 +1,9 @@
+include_role 'base'
+
+include_cookbook 'ghq'
+include_cookbook 'peco'
+include_cookbook 'tmux'
+include_cookbook 'jq'
+include_cookbook 'alp'
+include_cookbook 'myprofiler'
+include_cookbook 'slackcat'
