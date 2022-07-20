eab1d46e9081e0d68782ff62f8f5df5381561d34
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2022-07-20 21:13:37 +0900

    slackcatはダメって言われたので別のコマンドを使う

---
+++ cookbooks/sml/default.rb
@@ -0,0 +1,7 @@
+# https://github.com/furusax0621/slack-messenger-lite
+github_binary 'sml' do
+  repository 'furusax0621/slack-messenger-lite'
+  version '0.0.1'
+  archive 'sml_0.0.1_Linux_x86_64'
+  binary_path 'sml_0.0.1_Linux_x86_64'
+end
