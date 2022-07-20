24ede3896bebd819bcecf9398abd1eeff157e66a
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2022-07-20 21:25:19 +0900

    パラメータ調整

--- cookbooks/sml/default.rb
+++ cookbooks/sml/default.rb
@@ -1,7 +1,6 @@
 # https://github.com/furusax0621/slack-messenger-lite
 github_binary 'sml' do
   repository 'furusax0621/slack-messenger-lite'
-  version '0.0.1'
-  archive 'sml_0.0.1_Linux_x86_64'
-  binary_path 'sml_0.0.1_Linux_x86_64'
+  version 'v0.0.1'
+  archive 'sml_0.0.1_Linux_x86_64.tar.gz'
 end
